a708a611764b25a7506ddc4be4eaf3fda3fa45e1
Hayata Kitamura <58907957+cloud-hayata@users.noreply.github.com>
2025-05-25 11:13:09 +0900

    Update lambda_function.py

--- step4_ci_cd_build/lambda_function/lambda_function.py
+++ step4_ci_cd_build/lambda_function/lambda_function.py
@@ -1,6 +1,3 @@
+# Lambda関数にわざとエラーを仕込む
 def lambda_handler(event, context):
-    print("Lambda executed successfully")
-    return {
-        "statusCode": 200,
-        "body": "Hello DevOps!"
-    }
+    raise Exception("Forced Error for Alarm Test!")
